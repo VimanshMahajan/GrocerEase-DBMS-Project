52294459afda976ef724de2c2cd0760aa691f145
Anushka Srivastava <anushka22086@iiitd.ac.in>
2024-02-17 17:43:58 +0530

    queries

---
+++ GrocerEase/query.sql
@@ -0,0 +1,23 @@
+-- inventory searching
+
+-- cart order count
+
+-- balance check
+
+-- offline store closest to customer
+
+-- average rating for overall order
+
+-- select products below a certain cost
+
+-- item being ordered the most
+
+-- update address
+
+-- total cart cost
+
+-- delete from inventory
+
+-- group by categories + most sold product
+
+-- user's most frequent orders bought
