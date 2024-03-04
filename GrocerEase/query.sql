d89122e9ddb18e3a70165f2a0be4f45b18b15cce
Riya Gupta <132212858+riya22410@users.noreply.github.com>
2024-03-04 16:03:07 +0530

    Update query.sql

--- GrocerEase/query.sql
+++ GrocerEase/query.sql
@@ -14,10 +14,23 @@ FROM Inventory
 WHERE price < 99;
 
 -- item being ordered the most
-
+SELECT Orders.itemID, inventory.category, SUM(quantity) AS total_quantity
+FROM orders
+JOIN inventory ON Orders.itemID = inventory.itemID
+GROUP BY Orders.itemID, inventory.category
+ORDER BY total_quantity DESC
+limit 1
+;
 -- update address
+update customers
+set AddressLine1 = 'changed address' and AddressLine2 = ' to new address'
+where CustomerID = 1;
 
 -- total cart cost
+SELECT c.Customer_ID, SUM(c.Quantity * i.price) AS total_bill_price
+FROM cart c
+JOIN inventory i ON c.Item_ID = i.itemID
+GROUP BY c.Customer_ID
 
 -- delete from inventory
 
