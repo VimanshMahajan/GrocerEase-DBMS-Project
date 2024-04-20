c930f3446cd9e738cd96e8faa949a33aa87ce97c
sujalsuri <sujalsuri13@gmail.com>
2024-04-20 18:50:17 +0530

    Completed Transaction.sql hopefully

--- GrocerEase/data/transactions.sql
+++ GrocerEase/data/transactions.sql
@@ -1,6 +1,22 @@
 USE grocerease;
 
 -- CONFLICTING TRANSACTIONS
+-- When Two users buys at the same time
+START TRANSACTION;
+SELECT Quantity INTO @Cart_quantity from cart where Cart_ID = 1 and Item_ID = 1;
+SELECT @Cart_quantity * price INTO @Cart_cost from inventory where itemID = 1;
+UPDATE inventory SET stock = stock - @Cart_quantity where itemID = 1;
+INSERT INTO Orders (CustomerID,AgentID,itemID,quantity,OrderStatus,ETA,OrderTime) VALUES (1,1,1,@Cart_quantity,'Delivered',5,NOW());
+UPDATE wallet SET Balance = Balance - @Cart_cost where CustomerID = 1;
+COMMIT;
+
+START TRANSACTION;
+SELECT Quantity INTO @Cart_quantity from cart where Cart_ID = 2 and Item_ID = 1;
+SELECT @Cart_quantity * price INTO @Cart_cost from inventory where itemID = 1;
+UPDATE inventory SET stock = stock - @Cart_quantity where itemID = 1;
+INSERT INTO Orders (CustomerID,AgentID,itemID,quantity,OrderStatus,ETA,OrderTime) VALUES (2,1,1,@Cart_quantity,'Delivered',5,NOW());
+UPDATE wallet SET Balance = Balance - @Cart_cost where CustomerID = 2;
+COMMIT;
 
 -- When admin updates the stock and user buys that item at the same time
 START TRANSACTION;
@@ -33,4 +49,22 @@ COMMIT;
 
 START TRANSACTION;
 UPDATE customers SET PhoneNumber = 9473289108 where CustomerID = 2;
+COMMIT;
+
+-- Two admin viewing statistics at the same time
+START TRANSACTION;
+Select * into @AgentInfo from DeliveryAgent;
+COMMIT;
+
+START TRANSACTION;
+Select * into @AgentInfo from DeliveryAgent;
+COMMIT;
+
+-- Two users registering at the same time
+START TRANSACTION;
+Select Customer_Password into @pass from Customers where CustomerID = 1;
+COMMIT;
+
+START TRANSACTION;
+Select Customer_Password into @pass from Customers where CustomerID = 2;
 COMMIT;
